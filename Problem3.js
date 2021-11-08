e988d92bcaf6a3f68710d9a783cabc3b5b1a160c
Willian Reinaldo <48501869+Willreinaldo@users.noreply.github.com>
2021-11-08 16:52:31 -0300

    Update Problem3.js

--- Problem3.js
+++ Problem3.js
@@ -1,22 +1,10 @@
-/*Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
-For inputArray = [3, 6, -2, -5, 7, 3], the output should be
-adjacentElementsProduct(inputArray) = 21.
-
-7 and 3 produce the largest product.
-Guaranteed constraints:
-    2 ≤ inputArray.length ≤ 10,
-    -1000 ≤ inputArray[i] ≤ 1000.
-    */
-function adjacentElementsProduct(inputArray) {
-    let j = 1;
-    let biggest = -1000;
-    for (let i = 0; i < inputArray.length-1; i++) {
-        let product = (inputArray[i]*inputArray[j])
-       if(product > biggest){
-           biggest = product;
-       }
-     product = 0;
-     j++;
+//Given the string, check if it is a palindrome.
+function checkPalindrome(inputString) {
+    let reverse =  inputString.split("").reverse().join("");
+    if(reverse===inputString){
+        return true;
     }
-    return biggest;
+    else{
+        return false;
+    }
 }
